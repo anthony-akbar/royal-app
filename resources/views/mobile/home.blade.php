af3eca86ffebfc5b82f6816ef5d7fcc8919d5717
Anthony Akbar <mamadjanovakbar@gmail.com>
2022-12-29 15:00:29 +0500

    First

---
+++ resources/views/mobile/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.mobile')
+@section('content')
+    <h2>This is home page for Mobile</h2>
+    <div style="height: 30000px;">
+
+    </div>
+@endsection
---
+++ resources/views/mobile/partials/footer-bar.blade.php
@@ -0,0 +1,7 @@
+<div id="footer-bar" class="footer-bar-1 pb-2 footer-bar-no-text footer-bar-detached">
+    <a href="index-components.html"><i class="bi bi-star-fill"></i></a>
+    <a href="index-pages.html"><i class="bi bi-heart-fill"></i></a>
+    <a href="index.html" class="active-nav"><i class="bi bi-house-fill"></i></a>
+    <a href="index-search.html"><i class="bi bi-search"></i></a>
+    <a href="#" data-menu="menu-settings"><i class="bi bi-gear-fill"></i></a>
+</div>
