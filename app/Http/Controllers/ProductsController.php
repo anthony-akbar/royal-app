a952dc684a7e242f55be2e4790d04b868f81ade4
Anthony Akbar <mamadjanovakbar@gmail.com>
2023-01-01 11:13:53 +0500

    Products

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
     public function index () {
-
+        $products = Product::all();
+        return view('mobile.products.index', compact('products'));
     }
-
 
 }
--- resources/views/mobile/partials/footer-bar.blade.php
+++ resources/views/mobile/partials/footer-bar.blade.php
@@ -1,7 +1,17 @@
 <div id="footer-bar" class="footer-bar-1 pb-2 footer-bar-no-text footer-bar-detached">
-    <a class="{{ request()->is('/clients') ? "active-nav" : "" }}" href="{{ route('clients') }}"><i class="bi bi-people-fill"></i></a>
-    <a class="{{ request()->is() ? "active-nav" : "" }}" href="index-pages.html"><i class="bi bi-heart-fill"></i></a>
-    <a class="{{ request()->is('/') ? "active-nav" : "" }}" href="{{ route('home') }}"><i class="bi bi-house-fill"></i></a>
-    <a class="{{ request()->is() ? "active-nav" : "" }}" href="index-search.html"><i class="bi bi-search"></i></a>
-    <a class="{{ request()->is() ? "active-nav" : "" }}" href="#" data-menu="menu-settings"><i class="bi bi-gear-fill"></i></a>
+    <a class="{{ request()->is('/clients') ? "active-nav" : "" }}" href="{{ route('clients') }}">
+        <i class="bi bi-people-fill"></i>
+    </a>
+    <a class="{{ request()->is('/products/*') ? "active-nav" : "" }}" href="{{ route('products') }}">
+        <i class="bi bi-bag-fill"></i>
+    </a>
+    <a class="{{ request()->is('/') ? "active-nav" : "" }}" href="{{ route('home') }}">
+        <i class="bi bi-house-fill"></i>
+    </a>
+    <a class="{{ request()->is() ? "active-nav" : "" }}" href="index-search.html">
+        <i class="bi bi-search"></i>
+    </a>
+    <a class="{{ request()->is() ? "active-nav" : "" }}" href="#" data-menu="menu-settings">
+        <i class="bi bi-gear-fill"></i>
+    </a>
 </div>
